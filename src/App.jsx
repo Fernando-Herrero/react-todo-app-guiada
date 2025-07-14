0401a78d53f237ec4ef7a7689a9aa46ecb181e12
Fernando Herrero <herrerofernando.94@gmail.com>
2025-07-14 13:58:04 +0200

    chore(app): add app title

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 
 export const App = () => {
-    return (
-        <div className="app-container">
-            App
-        </div>
-    );
+	return (
+		<div className="app-container">
+			<h1 className="app-title">📝 Mi lista de Tareas</h1>
+		</div>
+	);
 };
